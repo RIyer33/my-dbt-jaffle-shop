4289a1bf70597088269b2a43f18da2570f7b56be
riyer33@gmail.com <riyer33@gmail.com>
2026-07-05 19:50:03 +0000

    add source freshness

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -1,24 +1,21 @@
-with 
+with
 
-source as (
+    source as (select * from {{ source("stripe", "payment") }}),
 
-    select * from raw.stripe.payment
+    renamed as (
 
-),
+        select
+            id as payment_id,
+            orderid as order_id,
+            paymentmethod as payment_method,
+            status as payment_status,
+            amount as payment_amount,
+            created as payment_created,
+            _batched_at
 
-renamed as (
+        from source
 
-    select
-        id as payment_id,
-        orderid as order_id,
-        paymentmethod as payment_method,
-        status as payment_status,
-        amount as payment_amount,
-        created as payment_created,
-        _batched_at
+    )
 
-    from source
-
-)
-
-select * from renamed
+select *
+from renamed
