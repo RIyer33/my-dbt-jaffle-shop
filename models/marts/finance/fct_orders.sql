a0c58e40c914c6db8e7cd302baf623822d57f86d
riyer33@gmail.com <riyer33@gmail.com>
2026-07-05 16:06:55 +0000

    fixed fct_orders

--- models/marts/finance/fct_orders.sql
+++ models/marts/finance/fct_orders.sql
@@ -1,23 +1,30 @@
-with 
-orders as ( 
-
-    select * from {{ ref('stg_jaffle_shop__orders') }}
-
-),
-payments as
-(
-    select * from {{ref('stg_stripe__payments')}}
+with orders as  (
+    select * from {{ ref ('stg_jaffle_shop__orders' )}}
 ),
 
-customer_payments as (
-select 
-o.order_id,
-o.customer_id,
-sum(p.amount) as amount
-from orders as o
-inner join payments as p using (order_id)
-where p.status = 'success'
-group by o.order_id, o.customer_id
+payments as (
+    select * from {{ ref ('stg_stripe__payments') }}
+),
 
+order_payments as (
+    select
+        order_id,
+        sum (case when payment_status = 'success' then payment_amount end) as amount
+
+    from payments
+    group by 1
+),
+
+ final as (
+
+    select
+        orders.order_id,
+        orders.customer_id,
+        orders.order_date,
+        coalesce (order_payments.amount, 0) as amount
+
+    from orders
+    left join order_payments using (order_id)
 )
-select * from customer_payments
+
+select * from final
